24e6df3f708385bf79581e041b6d2aa3fe0241fe
Renato V. Mendes <renato@vmendes.com.br>
2014-05-17 16:16:28 -0300

    Recuperando lista de prestadores de serviço (JSON) usando URL local

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -46,101 +46,36 @@ angular.module('sosWeb.controllers', ['ui.bootstrap','ui.map','ui.event'])
 		
 	};
 }])
-.controller('PrestadoresCtrl', ['$scope', function($scope) {
+.controller('PrestadoresCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.maxRate = 10;
 	//var urlPrestadores = 'http://soservices.vsnepomuceno.cloudbees.net/prestador?callback=JSON_CALLBACK';
+
+	//Filter and order
 	$scope.orderProp = '-avaliacao';
 	$scope.orderBy = function (orderProp) {
 		$scope.orderProp = orderProp;
 	};
 
-	$scope.prestadores = [
-	    {
-			"nome": "Diogo Peixoto",
-			"email": "peixotoo@gmail.com",
-			"senha": "12345",
-			"cpf": "000.000.000-00",
-			"logradouro": "Rua Fulaninho de tal",
-			"numero": "xxx",
-			"complemento": "apartamento x",
-			"cep": "xxxxx-xxx",
-			"telefone": "XX XXXX-XXXX",
-			"avaliacao": 10,
-		},
-		{
-			"nome": "Renato V. Mendes",
-			"email": "renato@vmendes.com.br",
-			"senha": "mypass",
-			"cpf": "111.111.111-11",
-			"logradouro": "Rua Três",
-			"numero": 123,
-			"complemento": "",
-			"cep": "53400-123",
-			"telefone": "81 8800-0088",
-			"avaliacao": 7,
-		},
-		{
-			"nome": "Vilmar Nepomuceno",
-			"email": "vsnepomuceno@gmail.com",
-			"senha": "asdasd",
-			"cpf": "222.222.222-22",
-			"logradouro": "Rua XYZ",
-			"numero": 987,
-			"complemento": "aptº 303",
-			"cep": "54678-009",
-			"telefone": "81 4444-4554",
-			"avaliacao": 8,
-		},
-		{
-			"nome": "José Maria",
-			"email": "zemaria@hotmail.com",
-			"senha": "10101970",
-			"cpf": "333.333.333-33",
-			"logradouro": "Rua da esquina",
-			"numero": 1212,
-			"complemento": "",
-			"cep": "54678-309",
-			"telefone": "81 3443-4554",
-			"avaliacao": 3,
-		},
-		{
-			"nome": "Karina Oliveira",
-			"email": "vkoliveira@hotmail.com",
-			"senha": "asdasd",
-			"cpf": "222.222.222-22",
-			"logradouro": "Rua XYZ",
-			"numero": 987,
-			"complemento": "aptº 303",
-			"cep": "54678-009",
-			"telefone": "81 4444-4554",
-			"avaliacao": 5,
-		},
-		{
-			"nome": "Tiago Alencar",
-			"email": "tiago@hotmail.com",
-			"senha": "10101970",
-			"cpf": "333.333.333-33",
-			"logradouro": "Rua da esquina",
-			"numero": 1212,
-			"complemento": "",
-			"cep": "54678-309",
-			"telefone": "81 3443-4554",
-			"avaliacao": 7,
-		}
-	];
-
-	$scope.bigTotalItems = $scope.prestadores.length;
-	$scope.bigCurrentPage = 1;
+	//Pagination
 	$scope.itemsPerPage = 1;
-
 	$scope.setPage = function (pageNo) {
 		$scope.currentPage = pageNo;
 	};
-
 	$scope.pageChanged = function() {
 		$scope.currentPage
 	};
 
+	$scope.prestadores = [];
+
+	$http.get('json/prestadores.json').success(function(data) {
+
+    	$scope.prestadores = data;
+
+		$scope.bigTotalItems = $scope.prestadores.length;
+		$scope.bigCurrentPage = 1;
+	});
+
+	//Map
 	// function createMarker(prest) {
 	// 	var marker_prest = new google.maps.Marker({
 	//         position: new google.maps.LatLng(prest.endereco.latitude,prest.endereco.longitude),
@@ -156,7 +91,6 @@ angular.module('sosWeb.controllers', ['ui.bootstrap','ui.map','ui.event'])
         zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-
     //Markers should be added after map is loaded
     $scope.onMapIdle = function() {
         if ($scope.myMarkers === undefined){    
